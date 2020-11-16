4e50e74af380f72c58002c5f06142f660112b5c1
anibalsantos <anibalsantosgo@gmail.com>
2020-11-16 23:31:11 +0100

    ng

---
+++ PEC4_Ej5/PEC4Ej5/src/app/wine-item/wine-item.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import Food from '../model/food';
+import Wine from '../model/wine';
+
+@Component({
+  selector: 'app-wine-item',
+  templateUrl: './wine-item.component.html',
+  styleUrls: ['./wine-item.component.css'],
+})
+export class WineItemComponent implements OnInit {
+  public counter: number = 0;
+  public wine!: Wine;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.wine = new Wine(
+      'Protos',
+      '../../assets/wines/protos.png',
+      10,
+      2,
+      true,
+      [
+        new Food('solomillo', 500, false, false),
+        new Food('patatas', 300, true, false),
+      ]
+    );
+    this.counter = 0;
+  }
+
+  unavilableWine(): void {
+    this.wine.isOnSale = !this.wine.isOnSale;
+  }
+
+  increment(): void {
+    this.counter++;
+  }
+  decrement(): void {
+    if (this.counter === 0) return;
+    this.counter--;
+  }
+}
